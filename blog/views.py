a39d727f72ef9259a89af5f396163ffe69aa0b1e
Jeno <kalinskij.zhenya@gmail.com>
2021-11-05 18:38:33 +0000

    big update. added users app and a lot of styles

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Correy',
-        'title': 'Blog Post 1',
-        'content': 'First post conent',
-        'date_posted': '25 Sep 2021'
-    },
-    {
-        'author': 'Mac',
-        'title': 'Blog Post 2',
-        'content': 'Second post conent',
-        'date_posted': '25 Dec 2021'  
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+    
     
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})

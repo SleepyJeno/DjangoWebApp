9764ad1026e3d87a7032586ebcdb8dbdd853576b
Jeno <kalinskij.zhenya@gmail.com>
2021-11-06 13:34:27 +0000

    added create, update and delete post functionality. log in checks and some styling.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,19 @@
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from django.urls import path
 
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #creates /1,2,n individual pages for each post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]

58185c91396a9a7b54d9807d24995be7b28ef977
Jeno <kalinskij.zhenya@gmail.com>
2021-11-07 10:32:25 +0000

    added paginator, some dummy posts data and user posts page

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,15 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 from django.urls import path
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #creates /1,2,n individual pages for each post
